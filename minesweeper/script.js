a96bd9a5ea03b168244ed7f670963386a8ded6f0
luanakwon <93167577+luanakwon@users.noreply.github.com>
2023-10-01 14:52:07 +0900

    Add files via upload
    fixed bug, added game mode, right click, win/lose condition

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,13 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
     const board = document.getElementById('board');
-    const resetButton = document.getElementById('reset');
-    const gridSize = 10;
-    const numMines = 20;
+    const modes = document.querySelector('.modes');
+    const infoDiv = document.getElementById('flag_info');
+    let gridSize = 10;
+    let numMines = 12;
     let cells = [];
+    let numCellsFlagged = 0; // increase whe rightclick
+    let numEmptyFound = 0;
 
+    // Define difficulties per game mode
+    const gameModes = {
+        easy: {gridSize: 10, numMines: 12},
+        medium: {gridSize: 16, numMines: 40},
+        hard: {gridSize: 22, numMines: 99}
+    }
+    
+    // Function to update the number of mines based on the selected game mode
+    function updateGameMode() {
+        const selectedMode = document.querySelector('input[name="gameMode"]:checked').value;
+        gridSize = gameModes[selectedMode].gridSize;
+        numMines = gameModes[selectedMode].numMines;
+    }
+    
     // Initialize the game board
     function initializeBoard() {
         board.innerHTML = '';
+        let cellSize = Math.floor(400/gridSize-2);
+        board.style.gridTemplateColumns = `repeat(${gridSize}, ${cellSize}px)`;
+        board.style.gridTemplateRows = `repeat(${gridSize}, ${cellSize}px)`;
+        numCellsFlagged = 0;
+        numEmptyFound = 0;
+        infoDiv.innerHTML = 'Flag : '+numMines;
         cells = [];
         for (let i = 0; i < gridSize; i++) {
             const row = [];
@@ -39,32 +62,63 @@ document.addEventListener('DOMContentLoaded', () => {
     // Event listener for cell clicks
     board.addEventListener('click', (e) => {
         const cell = e.target;
-        if (!cell.classList.contains('revealed')) {
-            const row = parseInt(cell.getAttribute('data-row'));
-            const col = parseInt(cell.getAttribute('data-col'));
-            if (cell.classList.contains('mine')) {
-                // Game over
-                cell.style.backgroundColor = '#f00';
-                alert('Game over!');
-                revealMines();
-            } else {
-                const mineCount = countAdjacentMines(row, col);
-                cell.textContent = mineCount > 0 ? mineCount : '';
-                cell.classList.add('revealed');
-                if (mineCount === 0) {
-                    // Auto-reveal adjacent cells
-                    revealEmptyCells(row, col);
+        if (cell.classList.contains('cell') && !cell.classList.contains('flagged')){
+            if (!cell.classList.contains('revealed')) {
+                const row = parseInt(cell.getAttribute('data-row'));
+                const col = parseInt(cell.getAttribute('data-col'));
+                if (cell.classList.contains('mine')) {
+                    // Game over
+                    cell.style.backgroundColor = '#f00';
+                    revealMines();
+                    //alert('Game over!');
+                    //TODO disallow board clicks
+                    infoDiv.innerHTML = 'Game Over..';
+                } else {
+                    cell.style.backgroundColor = '#ccc';
+                    numEmptyFound += 1;
+                    const mineCount = countAdjacentMines(row, col);
+                    cell.textContent = mineCount > 0 ? mineCount : '';
+                    cell.classList.add('revealed');
+                    if (mineCount === 0) {
+                        // Auto-reveal adjacent cells
+                        revealEmptyCells(row, col);
+                    }
                 }
+                // Game Clear check
+                if(numEmptyFound + numMines === gridSize*gridSize) {
+                    //alert('GameClear');
+                    //TODO disallow board clicks
+                    infoDiv.innerHTML = '!!Congratulations!!'
+                }
+                console.log(numEmptyFound);
             }
         }
     });
+    
+    // Event listner for cell right click
+    board.addEventListener('contextmenu', (e) => {
+        e.preventDefault(); // Prevent the default context menu
+        const cell = e.target;
+        if (cell.classList.contains('cell')){
+            if (!cell.classList.contains('revealed')) {
+                // Add your custom logic here, e.g., flagging the cell
+                // You can toggle a flag class or perform any other action
+                // toggle flagged, inc/dec count 
+                let flagged = cell.classList.toggle('flagged');
+                numCellsFlagged += flagged ? 1 : -1;
+                infoDiv.innerHTML = 'Flag : ' + (numMines - numCellsFlagged);
+            }
+        }
+        
+    });
 
-    // Event listener for the reset button
-    resetButton.addEventListener('click', () => {
+    // Event listener for the gamemode group
+    modes.addEventListener('change', () => {
+        updateGameMode();
         initializeBoard();
         generateMines();
     });
-
+    
     // Count adjacent mines
     function countAdjacentMines(row, col) {
         let count = 0;
@@ -90,12 +144,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 const newCol = col + j;
                 if (newRow >= 0 && newRow < gridSize && newCol >= 0 && newCol < gridSize) {
                     const cell = cells[newRow][newCol];
-                    if (!cell.classList.contains('revealed')) {
-                        const mineCount = countAdjacentMines(newRow, newCol);
-                        cell.textContent = mineCount > 0 ? mineCount : '';
-                        cell.classList.add('revealed');
-                        if (mineCount === 0) {
-                            revealEmptyCells(newRow, newCol);
+                    //if not revealed nor flagged
+                    if(!cell.classList.contains('revealed')){
+                        if (!cell.classList.contains('flagged')) {
+                            cell.style.backgroundColor = '#ccc';
+                            numEmptyFound += 1;
+                            const mineCount = countAdjacentMines(newRow, newCol);
+                            cell.textContent = mineCount > 0 ? mineCount : '';
+                            cell.classList.add('revealed');
+                            if (mineCount === 0) {
+                                revealEmptyCells(newRow, newCol);
+                            }
                         }
                     }
                 }
@@ -109,11 +168,13 @@ document.addEventListener('DOMContentLoaded', () => {
             if (cell.classList.contains('mine')) {
                 cell.textContent = '';
                 cell.classList.add('revealed');
+                cell.style.backgroundColor = '#f00';
             }
         });
     }
 
-    // Initialize the game
+    // Initialize the game in Easy mode
+    updateGameMode();
     initializeBoard();
     generateMines();
 });
